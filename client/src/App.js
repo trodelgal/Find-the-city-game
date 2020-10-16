510dafb6235fbe372729b2617db57e827215eedc
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-10-17 02:02:27 +0300

    show map

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import WrappedMap from './components/Map';
-import './App.css';
+import React from "react";
+import Map from "./components/Map";
+import "./App.css";
 
 
 function App() {
+
   return (
-    <div className="App" style={{width: '100%', height: '100%'}}>
-      <WrappedMap
-        googleMapURL= {"https://maps.googleapis.com/maps/api/js?key=AIzaSyCMDctJ_CkM4qTAw5VXgpXg6uvmsSOIGh0&callback=initMap&libraries=&v=weekly"}
-        loadingElement= {<div id="1" style={{width:"100%", height:"80%"}}/>}
-        containerElement={<div id="2" style={{width:"100%", height:"80%"}}/>}
-        mapElement={<div  id="3" style={{width:"100%", height:"80%", hidden:false}}/>}
-        />
+    <div className="App" style={{ width: "100%", height: "100%" }}>
+      <Map
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&callback=initMap&libraries=&v=weekly`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          isMarkerShown={true}
+          
+      />
     </div>
   );
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import React from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-function Map(){
-  return(
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
-  </GoogleMap>
-  )
-} 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+function moveMarker(location, marker) {
+  console.log(location);
+  console.log("here");
+  console.log(marker.props.position);
+//   marker.props.position=location
+}
 
-export default WrappedMap;
+// let marker = new google.maps.Marker({
+//   position: { lat: -34.397, lng: 150.644 },
+//   draggable: false,
+//   animation: google.maps.Animation.DROP,
+//   clickable: false,
+//   icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+// });
+const marker = (
+  <Marker
+    position={{ lat: -34.397, lng: 150.644 }}
+    draggable={true}
+    // animation={google.maps.Animation.DROP}
+    clickable={true}
+    icon="http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+  />
+);
+const Map = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      onClick={(event) => moveMarker(event.latLng, marker)}
+    >
+      {props.isMarkerShown && marker}
+    </GoogleMap>
+  ))
+);
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default Map;
