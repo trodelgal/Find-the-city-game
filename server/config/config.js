815c6be91fabe471c37968e0f617fefee6a5d213
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-12-25 19:45:47 +0200

    try to deploy again

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
+    username: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || null,
+    database: process.env.MYSQL_DATABASE || "Google_Map_Game",
+    host: process.env.MYSQL_HOST || "localhost",
     dialect: "mysql",
     define: {
       underscored: true,
--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
+require("dotenv").config();
 const app = require('./app');
 
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_ROOT_PASSWORD);
+console.log(process.env.MYSQL_DATABASE);
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_HOST);
+console.log(process.env.SERVER_PORT);
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_DATABASE);
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_DATABASE);
+console.log(process.env.MYSQL_HOST);
 const port = process.env.SERVER_PORT || 8080
 
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
