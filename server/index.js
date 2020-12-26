615b46a462e84b34f734eb430f2a51ad08b11836
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-12-26 16:17:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ console.log(process.env.MYSQL_DATABASE);
 console.log(process.env.MYSQL_USER);
 console.log(process.env.MYSQL_HOST);
 console.log(process.env.SERVER_PORT);
-console.log(process.env.MYSQL_USER);
-console.log(process.env.MYSQL_PASSWORD);
-console.log(process.env.MYSQL_DATABASE);
-console.log(process.env.MYSQL_USER);
-console.log(process.env.MYSQL_PASSWORD);
-console.log(process.env.MYSQL_DATABASE);
-console.log(process.env.MYSQL_HOST);
+
 const port = process.env.SERVER_PORT || 8080
 
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
