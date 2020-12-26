cbfc4764f02093076df71bb24f4e9aa987a1a180
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-12-26 15:20:05 +0200

    try to deploy

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || null,
-    database: process.env.MYSQL_DATABASE || "Google_Map_Game",
-    host: process.env.MYSQL_HOST || "localhost",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
     define: {
       underscored: true,
@@ -13,10 +13,10 @@ module.exports = {
     logging: false,
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
+    username: process.env.MYSQL_USER_TEST,
+    password: process.env.MYSQL_PASSWORD_TEST,
+    database: process.env.MYSQL_DATABASE_TEST,
+    host: process.env.MYSQL_HOST_TEST,
     dialect: "mysql",
   },
   production: {
